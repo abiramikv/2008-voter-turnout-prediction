359370839654875e72302162970573b9eaa9af7d
Gavy Aggarwal <gavyaggarwal@gmail.com>
2017-02-04 21:03:45 -0800

    Data parsing

--- turnout.py
+++ turnout.py
@@ -2,18 +2,23 @@
 import csv
 import numpy as np
 
-def getData(filename):
-    headers = []
+def getInputData(filename):
+    outputLabel = "pes1"
+    inputLabels = ["id"]
+    headers = {}
     with open(filename, "rb") as f:
         reader = csv.reader(f, delimiter=",")
         for i, line in enumerate(reader):
             if i == 0:
-                headers = line
-    print headers
-    #         print 'line[{}] = {}'.format(i, line)
-    # data = np.loadtxt(filename, delimiter=",", skiprows=1, dtype=int)
-    # print(data, data[:][-1])
-    # return data
+                for j, s in enumerate(line):
+                    headers[s.lower()] = j
+            else:
+                break
+    data = np.loadtxt(filename, delimiter=",", skiprows=1, dtype=int)
+    y = data[:, headers[outputLabel]]
+    X = data[:, [headers[h] for h in inputLabels]]
+    print(X)
+    return X, y
 
-train_raw = getData("train_2008.csv")
-test_raw  = getData("test_2008.csv")
+X, y = getInputData("train_2008.csv")
+#test_raw  = getData("test_2008.csv")
